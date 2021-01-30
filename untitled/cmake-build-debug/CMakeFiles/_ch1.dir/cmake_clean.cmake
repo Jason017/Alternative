3401ebc2241ffec6069d3bbefc9c89658fbde1e8
gjason <46449309+Jason017@users.noreply.github.com>
2021-01-30 01:46:07 -0500

    Add files via upload

---
+++ untitled/cmake-build-debug/CMakeFiles/_ch1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_ch1.dir/main.cpp.o"
+  "_ch1"
+  "_ch1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_ch1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
